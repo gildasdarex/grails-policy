6b90edb3fa40f9c9286b18236c2337b5ecfdac35
Darex TOSSA <gildasdarex@gmail.com>
2016-08-25 15:33:07 +0100

    remove build folder

--- build/resources/main/META-INF/commands/CreatePolicy.groovy
+++
@@ -1,9 +0,0 @@
-description("Creates a new Policy") {
-    usage "grails create-policy [POLICY NAME]"
-    argument name:'Policy Name', description:"The name of the policy"
-}
-
-model = model( args[0] )
-render  template:"Policy.groovy",
-        destination: file( "grails-app/policies/$model.packagePath/${model.simpleName}Policy.groovy"),
-        model: model
