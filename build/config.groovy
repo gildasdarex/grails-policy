cf29c6cc71e600f8e8a8baa5c2b08b57b920ebd6
Darex TOSSA <gildasdarex@gmail.com>
2016-08-25 15:34:09 +0100

    remove config file from build

--- build/config.groovy
+++
@@ -1,8 +0,0 @@
-
-withConfig(configuration) {
-    inline(phase: 'CONVERSION') { source, context, classNode ->
-        classNode.putNodeMetaData('projectVersion', '0.1')
-        classNode.putNodeMetaData('projectName', 'grails-policy')
-        classNode.putNodeMetaData('isPlugin', 'true')
-    }
-}
